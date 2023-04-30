146d1cc172da874f58c807148bc08fb163b67c3e
Harshad <harshadnavsar@gmail.com>
2023-04-30 17:46:55 +0530

    updated qus 2

--- InversionCount.cpp
+++ InversionCount.cpp
@@ -21,13 +21,63 @@ void countInversion(int n){
   cout<<"Inversion Count: "<<inversioncount<<endl;        
 }
 
+// T:O(nlogn) S:O(n)
+int countInversion2(int arr[],int temp[],int left,int mid,int right){
+    int i = left,j = mid,k = left,count = 0;
+
+    while((i<=mid) && (j<=right)){
+      if(arr[i] <= arr[j])
+        temp[k++] = arr[i++];
+      else{
+        temp[k++] = arr[j++];
+        count = count + (mid - i);
+      }
+    }
+
+    while(i <= mid - 1)
+      temp[k++] = arr[i++];
+
+    while(j<= right)
+      temp[k++] = arr[j++];  
+    
+    for(int i=left;i<=right;i++)
+      arr[i] = temp[i];
+
+    return count;  
+
+}
+
+int getInversion(int arr[],int low,int high,int temp[]){
+    int mid = 0,count = 0;
+
+    if(high > low){
+
+      mid =( low + (high - low)/2);
+
+      count += getInversion(arr,low,mid,temp);
+      count += getInversion(arr,mid+1,high,temp);
+
+      count += countInversion2(arr,temp,low,mid+1,high);
+    }
+
+    return count;
+}
+
 
 int main(){
 
   int n;
   cout<<"Enter number of elements:"<<endl;
   cin>>n;
-  countInversion(n);
+  // countInversion(n);
 
+  int arr[n];
+  int temp[n];
+  cout<<"Enter array elements:"<<endl;
+  for(int i=0;i<n;i++)
+    cin>>arr[i];
+
+  int totalinversion = getInversion(arr,0,n-1,temp);
+  cout<<"Number of Inversion: "<<totalinversion<<endl;
   return 0;
 }
